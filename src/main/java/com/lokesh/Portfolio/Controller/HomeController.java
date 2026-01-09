e9d92250439da36b8ec55a87f1c0137daf9c9c6a
lokesh sharma <85381561+lokesh658@users.noreply.github.com>
2026-01-09 22:17:11 +0530

    i"Initial changes"

---
+++ src/main/java/com/lokesh/Portfolio/Controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.lokesh.Portfolio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+    @GetMapping("/skills")
+    public String skills(){
+        return "skills";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    @GetMapping("/projects")
+    public String projects(){
+        return "projects";
+    }
+}
---
+++ src/test/java/com/lokesh/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokesh.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
